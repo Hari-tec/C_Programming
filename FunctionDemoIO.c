f63ee516592e4492e5c08349b4b5b601c1dbde0e
Hariom Tirkamte <hariomtirkamte@gmail.com>
2025-10-05 00:20:22 +0530

    Taking IO from user

---
+++ FunctionDemoIO.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0, iAns = 0;
+
+    printf("Enter First No: \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter Second No: \n");
+    scanf("%d",&iValue2);
+
+    iAns = iValue1 + iValue2 ;
+
+    printf("Addition is : %d\n",iAns);
+
+
+
+    return 0;
+}
