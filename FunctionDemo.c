6cec385845a2941945b0ab7644a766641009938c
Hariom Tirkamte <hariomtirkamte@gmail.com>
2025-10-05 00:14:58 +0530

    Using Function in c

---
+++ FunctionDemo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int value1 = 0, value2 = 0, Ans = 0;
+
+    value1 = 10;
+    value2 = 11;
+
+    Ans = value1 + value2;
+
+    printf("Addition is :%d\n",Ans);
+
+
+    return 0;
+}
